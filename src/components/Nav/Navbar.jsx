7f0e9d54f6cb0cb2c98bbe23680df0055e95d752
Ignacio Herner <ignah71@gmail.com>
2022-08-17 15:57:30 -0300

    Trabajo practico Navbar

---
+++ src/components/Nav/Navbar.jsx
@@ -0,0 +1,17 @@
+import style from './Navbar.module.css'
+
+const Navbar = () => {
+  return (
+    <nav className={style.NavContainer}>
+        <h2 className={style.h2_ContainerNav}>House <span className={style.span_ContainerNav}>Bit</span> </h2>
+        <div>
+          <a className={style.a_ContainerNav} href="/">Home</a>
+          <a className={style.a_ContainerNav} href="/">Shop</a>
+          <a className={style.a_ContainerNav} href="/">About</a>
+          <a className={style.a_ContainerNav} href="/">Contact</a>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar;
