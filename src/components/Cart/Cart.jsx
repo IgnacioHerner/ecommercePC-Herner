9cbfa7a1ab2f3e9356ca45764b4fe4dbfbb409ca
Ignacio Herner <ignah71@gmail.com>
2022-09-22 01:29:30 -0300

    arregÃlando detalles

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -23,17 +23,17 @@ const Cart = () => {
           <img className={style.img} src={prod.img} alt={prod.title} />
           <h2 className={style.h2} >{prod.title}</h2>
           <h3>Cantidad: {prod.qty}</h3>
-          <h3>${prod.price}.</h3>
-          <ul>
-            <li>
-                <button onClick={() => removeItem(prod.id)}>Eliminar Producto</button>
+          <h3 className={style.price}>${prod.price}.</h3>
+          <ul className={style.list_style}>
+            <li className={style.li_btn_eliminar}>
+                <button className={style.btn_eliminar} onClick={() => removeItem(prod.id)}>Eliminar Producto</button>
             </li>
           </ul>
         </div>
       ))}
       
-    <h4>Total: ${total}</h4>
-    <button className={style.btnClear} onClick={clearCart}> ClearCart</button>
+      <h4 className={style.total}>Total: ${total}</h4>
+      <button className={style.btnClear} onClick={clearCart}> ClearCart</button>
     </div>
   )
 }
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -8,10 +8,10 @@ import style from './ItemDetail.module.css'
 
 
 
-const ItemDetail = ({item}) => {
+const ItemDetail = ({ item }) => {
 
   const [cantidad, setCantidad] = useState(0)
-  const {addToCart, getProductQuantity} = useContext(CartContext)
+  const { addToCart, getProductQuantity } = useContext(CartContext)
 
   const onAdd = (cantidad) => {
     setCantidad(cantidad)
@@ -22,15 +22,15 @@ const ItemDetail = ({item}) => {
 
   return (
     <div className={style.detail}>
-      <img src={item.img} alt={item.title}/>
+      <img src={item.img} alt={item.title} />
       <div className={style.info}>
         <h2 className={style.h2}>{item.title}</h2>
         <p className={style.price}>$ {item.price}</p>
         <p className={style.stock}>Stock disponible: {item.stock}</p>
 
         {cantidad === 0
-            ? (<ItemCount stock ={item.stock} initial={quantity} onAdd={onAdd}/>) 
-            : (<Link to="/cart">Ir al carrito</Link>)
+          ? (<ItemCount stock={item.stock} initial={quantity} onAdd={onAdd} />)
+          : (<Link to="/cart">Ir al carrito</Link>)
         }
       </div>
     </div>
